bb6b29dd0773ebbfa3e5f80ff7f0c8757faa6ca0
Brendan Viloria <bjviloria104@gmail.com>
2017-12-12 11:52:33 -0800

    [ops] initial database works with some completed controllers

--- db/models/projects.js
+++ db/models/projects.js
@@ -1,33 +1,30 @@
 module.exports = (Sequelize, DataTypes) => {
   const Projects = Sequelize.define('projects', {
+    name: {
+      type: DataTypes.STRING,
+    },
     description: {
       type: DataTypes.STRING,
-      allowNull: false,
     },
     headquarters: {
       type: DataTypes.STRING,
-      allowNull: false,
     },
     imageurl: {
       type: DataTypes.STRING,
-      allowNull: false,
     },
     techstack: {
       type: DataTypes.STRING,
-      allowNull: false,
     },
     websites: {
       type: DataTypes.STRING,
-      allowNull: false,
     },
     status: {
       type: DataTypes.STRING,
-      allowNull: false,
     },
   });
 
   Projects.associate = models => {
-    Projects.belongsToMany(models.users, { through: 'ProjectsUsers' });
+    Projects.belongsToMany(models.users, { through: 'projectsusers' });
   };
 
   return Projects;
