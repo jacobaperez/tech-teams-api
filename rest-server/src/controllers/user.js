f55ca11716df223621a7cedfa63b417895ce36fb
Jake Perez <jacobanthony.perez@gmail.com>
2017-12-14 16:25:27 -0800

    [ops] - finish login server side - begin signup server side

--- rest-server/src/controllers/user.js
+++ rest-server/src/controllers/user.js
@@ -1,24 +1,27 @@
-import { generateToken } from '../middleware/authentication';
-import firebase, {auth} from ''
+import {
+  generateToken
+} from '../middleware/authentication';
+import firebase, {
+  auth
+} from './firebase.config';
 
 
-const authUser = (req, res) => {
-  let name = req.body.name;
+export const authUser = (req, res) => {
+  // let name = req.body.name;
   let email = req.body.email;
   let password = req.body.password;
-  firebase.auth.signInWithEmailAndPassword(email, password)
-    .then( user => {
-      console.log('User is signed in', user);
+  // let arr = [name, email, password];
+  // res.status(200).send(arr);
+  auth.signInWithEmailAndPassword(email, password)
+    .then(user => {
+      // console.log('User is signed in', user);
+      const token = generateToken({email});
+      res.status(200).send(token);
       // do something with email;
 
-    })
-    // firebase authenticate
-      // if authenticated generate jwt,
-    // else send back error bish...
-  // catch (err) {
-  //   console.log('Error', err);
-  //   res.status(400).send(err);
-  // }
-}
+    }, err => {
+      console.log('there was an error loggin in the user', err);
+      res.status(204).send(err);
 
-exports.authUser = authUser;
+    });
+}
