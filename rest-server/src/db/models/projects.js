2c7b0aaf28b09a3ad496494caf1369ed644cd288
Jake Perez <jacobanthony.perez@gmail.com>
2017-12-19 15:40:01 -0800

    [ops] - more merges

--- rest-server/src/db/models/projects.js
+++ rest-server/src/db/models/projects.js
@@ -28,6 +28,7 @@ module.exports = (Sequelize, DataTypes) => {
 
   Projects.associate = (models) => {
     Projects.belongsToMany(models.users, { through: 'projectsusers' });
+    Projects.belongsToMany(models.positions, { through: 'positionsprojects' });
   };
 
   return Projects;
